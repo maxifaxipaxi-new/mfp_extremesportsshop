8f0e0e09c68613aaa339255b89c4ec2d5e99036e
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-19 21:39:11 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ use_experimental_fxv2_oal "yes"
 name "MFP_EXTREMESPORTSHOP"
 author "MFPSCRIPTS"
 description "Sportshop for FiveM, works with ESX, QBCore or Custom Framework."
-version "2.0.0"
+version "2.0.1"
 
 shared_script {
     "config/*.lua",
